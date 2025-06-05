f1b33b64fa95beafb33501b30657552660afdefb
D.Kahadze <davknet1@gmail.com>
2025-06-05 12:10:24 +0300

    first commit

---
+++ js/style.js
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       App-Impulse Image Optimizer
+ * Plugin URI:        https://github.com/yourusername/app-impulse-image-optimiser
+ * Description:       Optimises images automatically for improved performance and faster load times.
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            Your Name or Company Name
+ * Author URI:        https://yourwebsite.com/  ; Or use your GitHub profile: https://github.com/yourusername
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/yourusername/app-impulse-image-optimiser
+ * Text Domain:       app-impulse-image-optimiser
+ * Domain Path:       /languages
+ */
