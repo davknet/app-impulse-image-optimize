3fd4f328dc22c0ad9c6792289654002b23953cd6
D.Kahadze <davknet1@gmail.com>
2025-06-05 12:33:16 +0300

    created header of the plugin

--- app-impulse-image-optimizer.php
+++ app-impulse-image-optimizer.php
@@ -1,2 +1,19 @@
 <?php 
+/*
+ * Plugin Name:       App-Impulse Image Optimizer
+ * Plugin URI:        https://github.com/yourusername/app-impulse-image-optimizer
+ * Description:       Optimizes images automatically for improved performance and faster load times.
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      8.0
+ * Author:            David Kahadze
+ * Author URI:        https://github.com/davknet
+ * License:           GPL v2 or later
+ * License URI:       
+ * Update URI:        https://github.com/davknet/app-impulse-image-optimiser
+ * Text Domain:       app-impulse-image-optimizer
+ * Domain Path:       /languages
+ */
+
+
 
