1c623fd3e6e6c5ec03d8248b0c0fbcbc6bf39619
D.Kahadze <davknet1@gmail.com>
2025-06-09 15:00:43 +0300

    feat(core): added security check and autoload.php files

--- app-impulse-image-optimizer.php
+++ app-impulse-image-optimizer.php
@@ -25,5 +25,11 @@
       //   site_url() 
       //   home_url()
 
-     
+defined('ABSPATH') or die("Direct access to the script does not allowed");
 
+
+
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ){
+    require_once __DIR__ . '/vendor/autoload.php';
+}
