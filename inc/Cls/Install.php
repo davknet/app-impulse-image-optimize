e56a0bcc090ba21db5c420626305c692540f8f93
D.Kahadze <davknet1@gmail.com>
2025-06-09 14:57:26 +0300

    docs(install): add class-level comment to Install.php

--- inc/Cls/Install.php
+++ inc/Cls/Install.php
@@ -1,12 +1,29 @@
 <?php 
 namespace Inc\Cls;
 
-
+/**
+ * Class Install
+ *
+ * Handles the activation routine for the plugin.
+ *
+ * This class is responsible for tasks that need to run when the plugin is activated,
+ * such as creating necessary database tables, setting default options, and preparing
+ * the environment for first-time use.
+ *
+ * Usage:
+ * Typically called via the register_activation_hook() function.
+ *
+ *
+ * @package App-Impulse-Image_optimizer 
+ */
 
 
 
 
 class Install{
+
+
+
 
 
 }
