c4761f1653efb5f01c193c366fe3941b9145596a
D.Kahadze <davknet1@gmail.com>
2025-06-10 11:26:34 +0300

    feat: added starter  template for plugin init

--- app-impulse-image-optimizer.php
+++ app-impulse-image-optimizer.php
@@ -42,7 +42,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ){
  */
 
  error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1 );
 ini_set('log_errors', 1);
 
 // $log_path = plugin_dir_path( __FILE__ ) . 'logs/error_logs.log';
